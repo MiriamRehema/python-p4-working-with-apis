d0d7ee9229ce53dd4a6e7bf5f0215cedf394844a
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-20 12:35:17 +0300

    working with APIs

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -2,14 +2,16 @@
 import requests
 import json
 
+
 class GetPrograms:
 
   def get_programs(self):
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
-    return response.content
-
+    #return response.text   i have commented it out since it will return the API in not a friendly way and used the dumps method
+    json_content = json.loads(response.text)
+    print(json.dumps(json_content, indent=4, sort_keys=True))
 
 programs = GetPrograms().get_programs()
 print(programs)
