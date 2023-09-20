8dbe7fb10ada8c8cc072134bc86d58a0e6bf6589
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-20 13:47:17 +0300

    working with APIs

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,9 +9,25 @@ class GetPrograms:
     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
     response = requests.get(URL)
-    #return response.text   i have commented it out since it will return the API in not a friendly way and used the dumps method
+    return response.text   #i have commented it out since it will return the API in not a friendly way and used the dumps method
     json_content = json.loads(response.text)
     print(json.dumps(json_content, indent=4, sort_keys=True))
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def program_school(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+            programs_list.append(program["agency"])
+
+    return programs_list
+#comment this out
+#programs = GetPrograms().get_programs()
+#print(programs)
+
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
+
